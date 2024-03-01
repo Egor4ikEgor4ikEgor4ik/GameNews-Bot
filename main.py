62974d8d6db77cae4ad0ddb3d2d4090f7108d2bf
Egor4ikEgor4ikEgorik <prodolzitprodolzit@gmail.com>
2024-03-01 20:43:20 +0300

    Wrapped code to build post and and some bugs have been fixed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
 import telegram
+from textwrap import dedent
 
 load_dotenv()
 
@@ -18,42 +19,32 @@ def get_last_news_coopland():
     post_url = soup.find('a', class_="big-link")['href']
     return headline,description,picture_url,post_url
 
-
-
-
-
-
-
-
+def build_post_coopland(headline_coopland,description_coopland,post_url_coopland):
+    post_coopland_text = dedent(f'''{headline_coopland}  
+    {description_coopland}    
+Ссылка на источник: {post_url_coopland} ''')    
+    
+    
+    return post_coopland_text
 
 headline_coopland,description_coopland,picture_url_coopland,post_url_coopland = get_last_news_coopland()
-#print (headline_coopland,description_coopland,picture_url_coopland,post_url_coopland)
 
 response = requests.get(picture_url_coopland)
 with open(f'./images_coopland/bebra_coopland.png', "wb") as file:    
             file.write(response.content)
-post_coopland = (f'''{headline_coopland}
-
-{description_coopland}
-
-Ссылка на источник: {post_url_coopland}
-
-''')
 
 telegram_bot_token = os.getenv("BOT_TOKEN")
 telegram_chat_id = '@gamingnewsfornerds'
 
 bot = telegram.Bot(token=telegram_bot_token)
-bot.send_photo(chat_id=telegram_chat_id, photo=open('./images_coopland/bebra_coopland.png', 'rb'),caption=post_coopland)
-# def replace_o_to_A(old_text):
-#     new_text = old_text.replace('о', "А")
-#     return new_text
+bot.send_photo(chat_id=telegram_chat_id, photo=open('./images_coopland/bebra_coopland.png', 'rb'),caption=build_post_coopland(headline_coopland,description_coopland,post_url_coopland))
 
 
-# text1 = "Привет, меня зовут Егор"
-# result_text_good1 = replace_o_to_A(old_text=text1)
 
-# text2 = "Привет, меня зовут олег"
-# result_text_good2 = replace_o_to_A(old_text=text2)
-# print(result_text_good1)
-# print(result_text_good2)
+
+
+
+
+
+
+
